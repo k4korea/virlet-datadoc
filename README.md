8546e2ecf877147d1868b11c0329379a3fea3b01
k4korea <love7310@naver.com>
2022-12-10 21:24:28 +0900

    work

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# virlet-datadoc
-virtlet datadoc
+# virtlet
+1. start kube yaml
+  command: ["node"]
+  args: ["index.js"]    ######  deploy-rollout node 
+  
+2.  docker command
+  node index.js
---
+++ index.js
@@ -0,0 +1,35 @@
+var express =require('express');
+var fs = require('fs');
+var app = express();
+
+//import './tracer'; 
+
+const tracer = require('dd-trace').init();
+
+app.get('/index',function(req,res){
+    fs.readFile('index.html', function(error, data){
+      res.writeHead(200, {'Content-Type': 'text/html'} ) ;
+      res.end(data);
+    });
+    
+});
+
+app.get('/',function(req,res){
+    fs.readFile('main.html', function(error, data){
+      res.writeHead(200, {'Content-Type': 'text/html'})  ;
+      res.end(data);
+    });
+    
+});
+
+app.get('/imgs',function(req,res){
+    fs.readFile('virtlet4.jpg', function(error, data){
+      res.writeHead(200, {'Content-Type': 'text/html'})  ;
+      res.end(data);
+    });
+    
+});
+
+app.listen( 80, function(){
+    console.log('Server Start');
+})
